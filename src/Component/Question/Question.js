ce2e54f34987f9bfa5ffa1d72673a0e6fa44ceea
ProgrammerBayzid <mdbayzid2622002@gmail.com>
2022-09-29 16:36:34 +0600

    6th commit

---
+++ src/Component/Question/Question.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Question = () => {
+    return (
+        <div>
+            <div>
+                 <h1>Question: How Dose React Work</h1>
+                 <h1>Answe: React is a declarative, efficient, and flexible JavaScript library for building user interfaces. 'V' denotes the view in MVC. ReactJS is an open-source, component-based front end library responsible only for the view layer of the application.</h1>
+                 <h1>Question:Props And State Different</h1>
+                 <h1>Answe: </h1>
+                 <h1>Question: useEffect </h1>
+                 <h1>Answe: </h1>
+            </div>
+        </div>
+    );
+};
+
+export default Question;
