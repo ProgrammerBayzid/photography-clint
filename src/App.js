4f2b4d8311b38ec46682091e485c67e5139183e3
ProgrammerBayzid <mdbayzid2622002@gmail.com>
2022-10-12 02:53:45 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
 
+import { RouterProvider } from 'react-router-dom';
+import { router } from './Compo/Root';
 
 
 function App() {
-  return (
-    <h1>Hello</h1>
-  );
+
+   return (
+      <div>
+         <RouterProvider router={router}></RouterProvider>
+      </div>
+   );
 }
 
 
---
+++ src/Compo/Root.js
@@ -0,0 +1,36 @@
+import { async } from "q";
+import { createBrowserRouter } from "react-router-dom";
+import Blog from "./Blog";
+import Main from "./Main";
+import QuixDeteails from "./QuixDeteails";
+import Statistics from "./Statistics";
+import Topics from "./Topics";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+                element: <Topics></Topics>
+            },
+            {
+                path: '/quiz/:quizId',
+                loader: async ({ params }) => fetch(`https://openapi.programming-hero.com/api/quiz/${params.quizId}`),
+                element: <QuixDeteails></QuixDeteails>
+
+            },
+            {
+                path: '/statistics',
+                loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+                element: <Statistics></Statistics>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
+        ]
+    }
+])
