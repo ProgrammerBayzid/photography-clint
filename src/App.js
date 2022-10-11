a1ab36bdbf4ba6132b85dc8c23c0879a0328929e
ProgrammerBayzid <mdbayzid2622002@gmail.com>
2022-10-12 02:42:16 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import logo from './logo.svg';
+import './App.css';
 
-import { RouterProvider } from 'react-router-dom';
-import { router } from './Compo/Root';
 
 
 function App() {
-
-   return (
-      <div>
-         <RouterProvider router={router}></RouterProvider>
-      </div>
-   );
+  return (
+    <h1>Hello</h1>
+  );
 }
 
 
